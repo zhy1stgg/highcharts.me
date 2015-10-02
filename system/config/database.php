75f6f30d02d61a5bf8d66cac12cf282f9618cfe4
root <root@keshihua.org>
2015-10-02 18:56:02 +0800

    first commit

---
+++ system/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'localhost',
+  'username' => 'hc_wenda',
+  'password' => 'wenda.hcharts.cn.me',
+  'dbname' => 'highcharts',
+);
+$config['slave'] = false;
---
+++ uc_client/data/cache/apps.php
@@ -0,0 +1,39 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'OTHER',
+    'name' => 'Highcharts中文社区',
+    'url' => 'http://highcharts.me',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+      'extraurl' => '			',
+    ),
+    'recvnote' => '1',
+  ),
+  2 => 
+  array (
+    'appid' => '2',
+    'type' => 'OTHER',
+    'name' => 'Highcharts中文网统一登录中心',
+    'url' => 'http://ucenter.hcharts.cn/',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+      'extraurl' => '			',
+    ),
+    'recvnote' => '1',
+  ),
+);
